ba73f2bb533539a906711a0b6758d566660ae5da
Brian <30425430+wendtbw@users.noreply.github.com>
2021-05-24 08:00:54 +0900

    Update basic_working_sample.py
    code from Ron - added code to pull multiple fields of data for each ticker and just cleaned up the sheet itself (formatting) and added titles (which are not generated by the code) in both English and Japanese in the spreadsheet.  added more comments in the code as well.

--- basic_working_sample.py
+++ basic_working_sample.py
@@ -7,7 +7,7 @@ import yfinance as yf
 # the spreadsheetID is the long string you find between the /d/ and /edit
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SAMPLE_SPREADSHEET_ID = '1PE8gBnr4bBujov502OUAfLpMFFZdT-yS5D042tEGRkQ'
-SAMPLE_RANGE_NAME = 'Sheet2!B2'
+SAMPLE_RANGE_NAME = 'Sheet2!B3'
 ​
 def main():
     """This is a fairly simple working example to look up stock prices
@@ -19,6 +19,7 @@ def main():
 ​
        The first time running this, I had more complex code to
        establish authorization.  Code came from Google Sheets API pages.
+       # https://developers.google.com/sheets/api/quickstart/python/
        Ive since removed said code now that the token
        file (token.json) has been created.
     """
@@ -32,17 +33,25 @@ def main():
                                 range=SAMPLE_RANGE_NAME).execute()
 ​
 ​
-    ticker_list = ["TSLA", "AAPL", "LB", "EXPE", "PXD", "MCHP", "CRM", "GOOG", "NRG", "ORCL", "NOW"]
+    ticker_list = ["6758.T", "9983.T", "9432.T", "6098.T", "7974.T", "8035.T", "9434.T", "7267.T", "9984.T", "7203.T", "4755.T"]
+#    ticker_list = ["TSLA", "AAPL", "LB", "EXPE", "PXD", "CRM", "GOOG", "ORCL", "4755.T", "9984.T", "7203.T"]
+​
+    metrics = ['previousClose', 'currency', 'shortName', 'twoHundredDayAverage', 'trailingPE',
+                       'trailingAnnualDividendYield', 'payoutRatio', 'priceToSalesTrailing12Months']
 ​
     data_to_write = []
+    metric_values = []
 ​
-    for t in ticker_list:
-        mydict = yf.Ticker(t).info
-        print()
-        previous_close = mydict.get('previousClose')
-        print(t, previous_close)
-        data_to_write.append([t, previous_close])
-​
+    for t in ticker_list:                                   # for each ticker in the ticker list....
+        mydict = yf.Ticker(t).info                          # get a dictionary of numerous metric values from yahoo finance
+        metric_values.append(t)                             # add the ticker as the first column in metric_values for ticker t
+        for metric in metrics:                              # for each metric of ticker t we want to write into the google sheet....
+            metric_values.append(mydict.get(metric))        # append the value of the metric to metric_values
+        print(metric_values)
+        data_to_write.append(metric_values)                 # take the list of metric values we obtained for ticker t, and append it
+                                                            # to data_to_write which is a list of lists (like a 2 dimensional array)
+                                                            # that we will eventually write into the google sheet below
+        metric_values = []                                  # reset metric_values for the next ticker
     print(data_to_write)
 ​
     request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME,
